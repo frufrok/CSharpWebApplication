31a7d3bee41150ad014b49dd781ac07df1faa71b
frufrok <frufrok@gmail.com>
2024-08-08 20:31:33 +0300

    Добавлен контроллер продуктов.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using CSharpWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpWebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("getProducts")]
+        public IActionResult GetProducts()
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    var products = context.Products.Select(x => new Product()
+                    {
+                        ID = x.ID,
+                        Name = x.Name,
+                        Description = x.Description,
+                        Price = x.Price
+                    });
+                    return Ok(products);
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+        [HttpPost("putProducts")]
+        public IActionResult PutProducts([FromQuery] string name, string description, double price)
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    if (!context.Products.Any(x => x.Name.ToLower().Equals(name.ToLower())
+                    {
+                        context.Add(new Product()
+                        {
+                            Name = name,
+                            Description = description,
+                            Price = price
+                        });
+                        context.SaveChanges();
+                        return Ok();
+                    }
+                    else 
+                    {
+                        return StatusCode(409);
+                    }
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # CSharpWebApplication
+
+## Урок 1. ASP.NET база
+
+### Задание
+
+Доработайте контроллер, дополнив его возможностью удалять группы и продукты, а также задавать цены. Для каждого типа ответа создайте свою модель.
+
+### Решение
+
+Созданы сущности и контекст базы данных.
+
+Выполнена миграция командой:
+
+```bash
+dotnet ef migrations add InitialCreate --context ProductContext
+```
+
+Изменения внесены в базу данных командой:
+```bash
+dotnet ef database update
+```
+
+База данных успешно создана:
+![Таблицы БД PostgreSQL](https://downloader.disk.yandex.ru/preview/57fdd9082d9dd6bdf5458a41ae0316fdd814b653b609ce46351c2fce4ef9b8d2/66b53430/2WerxUfO1mgUR2UYFACGWWhLhZ4UFVG9ahxXkuwD7ueF7yVj5TKyXMo2IpwaZ5FkC21uvXXJL40b405OgmaktA%3D%3D?uid=0&filename=2024-08-08_20-08-41.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=0&tknv=v2&size=2048x2048)
+
+Созданы контроллеры для продуктов и категорий.
