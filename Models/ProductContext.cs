effa636dc4952cceaaf8e96b21cef174dcae651e
frufrok <frufrok@gmail.com>
2024-08-08 19:41:23 +0300

    Add project files.

---
+++ Models/ProductContext.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CSharpWebApplication.Models
+{
+    public class ProductContext : DbContext
+    {
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Storage> Storages { get; set; }
+        public virtual DbSet<ProductStorage> ProductStorages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(@
+            //optionsBuilder
+            //optionsBuilder.UseLazyLoadingProxies().UseNpgsql("Host=localhost;Username=postgres;Password=example;Database=Product");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(e =>
+            {
+                e.ToTable("Products");
+
+                e.HasKey(x => x.ID).HasName("ID");
+                e.Property(x => x.ID).HasColumnName("ID");
+
+                e.HasIndex(x => x.Name).IsUnique();
+                e.Property(x => x.Name)
+                    .HasColumnName("Name")
+                    .HasMaxLength(255)
+                    .IsRequired(true);
+                
+                e.Property(x => x.Description)
+                    .HasColumnName("Description")
+                    .HasMaxLength(1023)
+                    .IsRequired(false);
+
+                e.Property(x => x.Price)
+                    .HasColumnName("Price")
+                    .IsRequired(true);
+
+                e.HasOne(x => x.Category)
+                    .WithMany(y => y.Products)
+                    .HasForeignKey(x => x.ID)
+                    .HasConstraintName("CategoryToProduct");
+            });
+
+
+            modelBuilder.Entity<Category>(e =>
+            {
+                e.ToTable("Categories");
+
+                e.HasKey(x => x.ID).HasName("CategoryID");
+                e.Property(x => x.ID).HasColumnName("ID");
+                
+                e.HasIndex(x => x.Name).IsUnique();
+                e.Property(x => x.Name)
+                   .HasColumnName("Name")
+                   .HasMaxLength(255)
+                   .IsRequired(true);
+
+            });
+
+
+            modelBuilder.Entity<Storage>(e =>
+            {
+                e.ToTable("Storages");
+
+                e.HasKey(e => e.ID).HasName("StorageID");
+                e.Property(e => e.ID).HasColumnName("ID");
+
+                e.HasIndex(x => x.Name).IsUnique();
+                e.Property(x => x.Name)
+                    .HasColumnName("Name")
+                    .HasMaxLength(255)
+                    .IsRequired(true);
+
+            });
+
+            modelBuilder.Entity<ProductStorage>(e =>
+            {
+                e.ToTable("ProductStorages");
+
+                e.HasKey(x => x.ID).HasName("ProductStorageID");
+                e.Property(x => x.ID).HasColumnName("ID");
+
+                e.Property(x => x.ProductID)
+                    .HasColumnName("ProductID")
+                    .IsRequired(true);
+                
+                e.Property(x => x.StorageID)
+                    .HasColumnName("StorageID")
+                    .IsRequired(true);
+
+                e.HasOne(x => x.Product)
+                    .WithMany(y => y.ProductStorages)
+                    .HasForeignKey(x => x.ProductID)
+                    .HasConstraintName("ProductToProductStorage");
+
+                e.HasOne(x => x.Storage).WithMany(y => y.ProductStorages)
+                    .HasForeignKey(x => x.StorageID)
+                    .HasConstraintName("StorageToProductStorage");
+
+            });
+        }
+    }
+}
